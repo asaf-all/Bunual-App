4248ddbad05a2533311f60aa1b89887874161d62
Asaf01 <77666792+AsafHacioglu@users.noreply.github.com>
2021-09-26 18:05:23 +0400

    There is no significant change

--- app/src/main/java/com/nomanim/bax/ui/fragments/newAnnouncementActivity/UserFragment.kt
+++ app/src/main/java/com/nomanim/bax/ui/fragments/newAnnouncementActivity/UserFragment.kt
@@ -5,7 +5,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.nomanim.bax.R
 import com.nomanim.bax.databinding.FragmentUserBinding
 
 class UserFragment : Fragment() {
